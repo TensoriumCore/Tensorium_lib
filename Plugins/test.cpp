c548495f5882ddcbfb340e359186129f4983ad3c
Louis Touzalin <ltouzali@2A5.42angouleme.fr>
2025-05-08 18:23:46 +0200

    IR LLVM analysis for restricted pointers test via custom pragmas

--- Plugins/test.cpp
+++ Plugins/test.cpp
@@ -1,20 +1,29 @@
 #include "../includes/Morpheus/Morpheus.hpp"
+#include <iostream>
 
 void saxpy(int n, float *A, float *B, float k)
 {
-    #pragma morpheus restrict(A,B)
+    #pragma morpheus restrict(A,B)  
     for (int i = 0; i < n; ++i)
-        B[i] += k * A[i];  
+        B_re[i] += k * A_re[i];  
 }
-int main() {
-#pragma morpheus dispatch
-	int n = 10;
-	float A = 0.2;
-	float B = 0.22;
-	float k = 12;
-	
-	saxpy(n, &A, &B, k);
 
+int main()
+{
+#pragma morpheus dispatch
+    const int n = 10;
+    float A[n], B[n];
+    for (int i = 0; i < n; ++i) {
+        A[i] = 0.2f * i;
+        B[i] = 0.22f * i;
+    }
+
+    float k = 12.0f;
+    saxpy(n, A, B, k);
+
+    for (float v : B) std::cout << v << ' ';
+    std::cout << '\n';
     return 0;
 }
+
 
