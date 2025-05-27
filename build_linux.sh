a1e9a9e43a88ebd5e8053760b822973139538e56
Louis Touzalin <louistouz@icloud.com>
2025-05-27 20:31:05 +0200

    eheh

---
+++ build_linux.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p pybuild
+cd pybuild
+
+cmake .. \
+    -DPYTHON_EXECUTABLE=$(which python) \
+    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=.
+
+make -j$(nproc)
+
+SO_FILE=$(find . -maxdepth 1 -name "*.so" | head -n 1)
+
+if [ -z "$SO_FILE" ]; then
+    echo "Erreur : aucun .so généré"
+    exit 1
+fi
+
+mv "$SO_FILE" ../pysrc/tensorium/
